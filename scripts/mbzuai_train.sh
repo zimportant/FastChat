487e7174d2b9cf1b22a46efb5845b7566cd5fffa
zhisbug <zhisbug@gmail.com>
2023-04-12 14:16:43 -0700

    update

--- scripts/mbzuai_train.sh
+++ scripts/mbzuai_train.sh
@@ -1,7 +1,7 @@
 MODEL_SIZE=7
 SEQ_LEN=256
 GC_SCALE=1
-DATE=20230303
+DATE=20230410
 echo "Training with seq_len=${SEQ_LEN} and gc_scale=${GC_SCALE}"
 PER_DEVICE_BATCH_SIZE=$((256 * $GC_SCALE / $SEQ_LEN))
 NUM_NODES=1
@@ -9,6 +9,38 @@ HOST_ADDR=127.0.0.1
 # Hack copy it once to make it faster later
 #mkdir -p ~/.checkpoints
 #gsutil -m cp -r /artifacts/chatbot/${MODEL_SIZE}b/sharegpt-${DATE}-seq-${SEQ_LEN} ~/.checkpoints
+
+torchrun \
+  --nnodes=$NUM_NODES \
+  --nproc_per_node=4 \
+  --master_port=12375 \
+  --master_addr=$HOST_ADDR \
+  --node_rank=${SKYPILOT_NODE_RANK} \
+  $TRAIN_SCRIPT \
+  --model_name_or_path ~/hf-output/llama-${MODEL_SIZE}b \
+  --data_path /data/sharegpt/sharegpt_20230322_clean_lang_split.json \
+  --bf16 True \
+  --output_dir $LOCAL_CKPT_PATH \
+  --num_train_epochs 3 \
+  --per_device_train_batch_size $PER_DEVICE_BATCH_SIZE \
+  --per_device_eval_batch_size $PER_DEVICE_BATCH_SIZE \
+  --gradient_accumulation_steps $((128 * 512 / $SEQ_LEN / $PER_DEVICE_BATCH_SIZE / $NUM_NODES / $SKYPILOT_NUM_GPUS_PER_NODE)) \
+  --evaluation_strategy "no" \
+  --save_strategy "steps" \
+  --save_steps 1200 \
+  --save_total_limit 10 \
+  --learning_rate 2e-5 \
+  --weight_decay 0. \
+  --warmup_ratio 0.03 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --fsdp "full_shard auto_wrap" \
+  --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+  --tf32 True \
+  --model_max_length ${SEQ_LEN} \
+  --gradient_checkpointing True \
+  --lazy_preprocess True
+
 torchrun \
   --nnodes=$NUM_NODES \
   --nproc_per_node=4 \
